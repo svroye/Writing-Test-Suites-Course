d6218fe91d460c8f94bb5363393552bbb3e361a4
Van Roye <steven.van-roye@sogeti.com>
2017-07-02 01:19:29 +0200

    Add test case for adding new contact

---
+++ spec/AddressBookSpec.js
@@ -0,0 +1,11 @@
+describe('Address Book', function(){
+
+	it('should be able to add a contact', function(){
+		var addressBook = new AddressBook();
+		var newContact = new Contact();
+
+		addressBook.addContact(newContact);
+		
+		expect(addressBook.getContact(0).toBe(newContact));
+	});
+});
